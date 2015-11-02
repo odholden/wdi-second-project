d3712341e2b0b3a4067390cafb9a85e73f86fdb0
odholden <odholden@gmail.com>
2015-11-02 09:28:54 +0000

    seed data for users complete, adding seed data for cities

--- skate_app/db/seeds.rb
+++ skate_app/db/seeds.rb
@@ -1,11 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
+# USERS
 
 u1 = User.create(email: 'odholden@gmail.com', password: 'password', image: 'ollie.png', username: 'ollieholden', fame:'100')
 u2 = User.create(email: 'guus@who.com', password: 'password', image: 'guus.png', username: 'guuswho', fame:'300')
@@ -14,6 +7,30 @@ u4 = User.create(email: 'gyom@french.com', password: 'password', image: 'gyom.pn
 u5 = User.create(email: 'tony@hawk.com', password: 'password', image: 'tony.png', username: 'tonyhawk', fame:'35000')
 u6 = User.create(email: 'eric@koston.com', password: 'password', image: 'eric.png', username: 'erickoston', fame:'10000')
 u7 = User.create(email: 'tony@alva.com', password: 'password', image: 'alva.png', username: 'tonyalva', fame:'13000')
-u8 = User.create(email: 'odholden@gmail.com', password: 'password', image: 'ollie.png', username: 'ollieholden', fame:'100')
-u9 = User.create(email: 'odholden@gmail.com', password: 'password', image: 'ollie.png', username: 'ollieholden', fame:'100')
-u10 = User.create(email: 'odholden@gmail.com', password: 'password', image: 'ollie.png', username: 'ollieholden', fame:'100')
+
+# VEHICLES
+
+v_bmx        = Vehicle.create(name: 'bmx')
+v_skateboard = Vehicle.create(name: 'skateboard')
+v_blades     = Vehicle.create(name: 'blades')
+v_scooter    = Vehicle.create(name: 'scooter')
+v_skiing     = Vehicle.create(name: 'skiing')
+v_snowboard  = Vehicle.create(name: 'snowboard')
+
+# COUNTRY
+
+# CITY
+
+city_la       =
+city_nyc      = 
+city_sanfran  = 
+city_portland = 
+city_chicago  = 
+city_seattle  = 
+city_philly   = 
+city_sandiego = 
+city_miami    = 
+city_wash     = 
+city_fortlaud = 
+
+
