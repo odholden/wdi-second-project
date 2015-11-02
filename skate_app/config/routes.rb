bf50cffa62789b7764accdf123dbb62d2e7914af
odholden <odholden@gmail.com>
2015-11-02 00:32:28 +0000

    added controller for homepage

--- skate_app/config/routes.rb
+++ skate_app/config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+
+  get 'static/home'
+
+  root 'static#home'
+
   resources :videos
   devise_for :users
   resources :users, only: :show 
   get '/users', to: 'users#index'
+
 end
