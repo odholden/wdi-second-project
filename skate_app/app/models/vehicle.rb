c1c105ed31be0097d799a45bb09d31ff77eb3a3a
odholden <odholden@gmail.com>
2015-11-02 01:08:26 +0000

    added associations

--- skate_app/app/models/vehicle.rb
+++ skate_app/app/models/vehicle.rb
@@ -1,2 +1,3 @@
 class Vehicle < ActiveRecord::Base
+  has_many :videos
 end
--- skate_app/app/models/video.rb
+++ skate_app/app/models/video.rb
@@ -2,4 +2,5 @@ class Video < ActiveRecord::Base
   belongs_to :user
   belongs_to :vehicle
   belongs_to :city
+  has_many   :tricks
 end
