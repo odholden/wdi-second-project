ffa2d536aba6abe59ada925e8529eda1461633e8
odholden <odholden@gmail.com>
2015-11-04 12:08:48 +0000

    added column image to cities table

--- skate_app/db/schema.rb
+++ skate_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104005507) do
+ActiveRecord::Schema.define(version: 20151104120819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20151104005507) do
     t.integer  "country_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   add_index "cities", ["country_id"], name: "index_cities_on_country_id", using: :btree
