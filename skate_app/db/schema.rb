ccfcdc316147abb10d12561a45575eab3b685eae
odholden <odholden@gmail.com>
2015-11-04 00:40:47 +0000

    removed image from users table to prepare for carrierwave

--- skate_app/db/schema.rb
+++ skate_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103165907) do
+ActiveRecord::Schema.define(version: 20151104003905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,7 +65,6 @@ ActiveRecord::Schema.define(version: 20151103165907) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.string   "image"
     t.string   "username"
     t.integer  "fame"
     t.datetime "date_joined"
