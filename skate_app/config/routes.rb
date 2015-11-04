0a4f7562cdad10134e524cb90936a85a0bce54dc
odholden <odholden@gmail.com>
2015-11-04 12:39:56 +0000

    created countries pages

--- skate_app/config/routes.rb
+++ skate_app/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'countries/index'
+
+  get 'countries/show'
+
   root 'static#home'
 
   resources :videos do
--- skate_app/db/schema.rb
+++ skate_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104120819) do
+ActiveRecord::Schema.define(version: 20151104123814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20151104120819) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   create_table "tricks", force: :cascade do |t|
