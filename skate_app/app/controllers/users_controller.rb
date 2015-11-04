49ab7069da42eba81ba8bdf609136b29f4c198c6
odholden <odholden@gmail.com>
2015-11-04 02:09:02 +0000

    added full cumulative fame functionality

--- skate_app/app/controllers/users_controller.rb
+++ skate_app/app/controllers/users_controller.rb
@@ -2,12 +2,13 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @fame = 0
   end
 
   def show
     @user   = User.find(params[:id])
     @videos = @user.videos
-    @fame   = 0
+    @fame = 0
     @videos.each do |video|
       upvotes = video.get_likes.size
       @fame = @fame + upvotes
