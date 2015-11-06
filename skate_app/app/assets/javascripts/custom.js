b703b48d3db82572f101f91d82168c81ae5c5aa9
odholden <odholden@gmail.com>
2015-11-06 01:45:34 +0000

    added footer image

--- skate_app/app/assets/javascripts/custom.js
+++ skate_app/app/assets/javascripts/custom.js
@@ -1,9 +1,6 @@
 $(function() {
   $('.slide').each(function() {
-    console.log('working');
-
     var index = $(this).index() + 1;
-    console.log(index);
     $(this).hide().delay(200*index).slideDown();
   });
 });
