a30cffa709c86637e8f297cc610caf594c231a93
odholden <odholden@gmail.com>
2015-11-06 02:14:50 +0000

    added fadein

--- skate_app/app/assets/javascripts/custom.js
+++ skate_app/app/assets/javascripts/custom.js
@@ -1,6 +1,12 @@
 $(function() {
+  $('body').hide().fadeIn(1000);
+})
+
+$(function() {
   $('.slide').each(function() {
-    var index = $(this).index() + 1;
-    $(this).hide().delay(200*index).slideDown();
+    var index = $(this).index();
+    $(this).hide().delay(200*index).slideDown(800);
   });
 });
+
+
